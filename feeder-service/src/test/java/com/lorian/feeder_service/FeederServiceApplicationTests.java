d54cead53cbefcd583e7cb6ebe8ee22d53e95098
Guw2 <gustafar79@gmail.com>
2025-06-20 19:57:32 -0300

    1st Commit - Starting Project

---
+++ feeder-service/src/test/java/com/lorian/feeder_service/FeederServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorian.feeder_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sensor-service/src/test/java/com/lorian/sensor_service/SensorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorian.sensor_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
